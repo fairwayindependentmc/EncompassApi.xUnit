88ee29f997536f39d76b85a0dd6d999dbb37085e
Michael Seifi <michael.seifi@fairwaymc.com>
2021-04-30 08:31:57 -0500

    Add Seed for Loan Documents

--- EncompassApi.xUnit/Seeds/LoanDocuments.cs
+++ EncompassApi.xUnit/Seeds/LoanDocuments.cs
@@ -24,6 +24,26 @@ namespace EncompassApi.xUnit.Seeds
             }
             // Load the file
             var fileData = File.ReadAllText(fullPath);
-            return JsonConvert.DeserializeObject<JObject[]>(fileData);        }
+            return JsonConvert.DeserializeObject<JObject[]>(fileData);
+        }
+
+        public static JObject GetLoanDocumentSeed()
+        {
+            var fullPath = $"Payloads/LoanDocuments.json";
+
+            // Get the absolute path to the JSON file
+            var path = Path.IsPathRooted(fullPath)
+                ? fullPath
+                : Directory.GetCurrentDirectory() + "/" + fullPath;
+
+            if (!File.Exists(path))
+            {
+                throw new ArgumentException($"Could not find file at path: {path}");
+            }
+            // Load the file
+            var fileData = File.ReadAllText(fullPath);
+            var objs = JsonConvert.DeserializeObject<JObject[]>(fileData);
+            return objs[0];
+        }
     }
 }
