30d553c22e229d8ee5a26f07af66178b43b36d1d
Michael Seifi <michael.seifi@fairwaymc.com>
2021-05-03 20:41:55 -0500

    Add ObjectArrayExtenstions

---
+++ EncompassApi.xUnit/Extensions/ObjectArrayExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncompassApi.xUnit.Extensions
+{
+    public static class ObjectArrayExtensions
+    {
+        public static IEnumerable<TObject> ChangeProperty<TObject>(this IEnumerable<TObject> objs, string PropertyName, object newValue)
+        {
+            foreach (var obj in objs)
+            {
+                var p = obj.GetType().GetProperty(PropertyName);
+                p.SetValue(obj, newValue);
+
+            }
+
+            return objs;
+        }
+    }
+}
