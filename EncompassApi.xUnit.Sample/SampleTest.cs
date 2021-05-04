fcff1f6937d963704565c1667b4f72adfb7600ec
Michael Seifi <michael.seifi@fairwaymc.com>
2021-05-04 17:14:42 -0500

    Add Sample project

---
+++ EncompassApi.xUnit.Sample/SampleTest.cs
@@ -0,0 +1,53 @@
+﻿using EncompassApi.xUnit.Services;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EncompassApi.xUnit.Sample
+{
+    public class SampleTest
+    {
+        private readonly ITestOutputHelper _outputWriter;
+        private readonly IMockedEncompassHttpClientService _mockedEncompassClient;
+
+        public SampleTest(
+            ITestOutputHelper outputWriter,
+            IMockedEncompassHttpClientService mockedEncompassHttpClient)
+        {
+            outputWriter.WriteLine("### SampleTest initiating! ###");
+            _outputWriter = outputWriter;
+            _mockedEncompassClient = mockedEncompassHttpClient;
+        }
+
+        [Fact]
+        public void TestMe()
+        {
+            var mockedLoanId = Guid.NewGuid().ToString();
+            var mockedDocumentId = Guid.NewGuid().ToString();
+            var mockedLoanDocument = Payloads.Helper.GetLoanDocument();
+            // Mock client and 
+            var source = _mockedEncompassClient
+                //.SetClientApiResponse((sender, args) =>
+                //{
+                //    args.Should().NotBeNull(because: "Response is null!");
+                //})
+                .SetupResponseMessage(resp =>
+                {
+                    resp.StatusCode = System.Net.HttpStatusCode.OK;
+                    resp.Content = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(mockedLoanDocument));
+                }, testHeader: new KeyValuePair<string, string>("TestHeader", Faker.Lorem.GetFirstWord()))
+                .SetDocumentsApiResponseCallback(mockedLoanId, (sender, args) =>
+                {
+                    args.Should().NotBeNull(because: "Response is null!");
+                })
+                .GetDocumentAsync(mockedDocumentId).ConfigureAwait(false);
+
+            source.Should()
+                .NotBeNull();
+                
+        }
+    }
+}
---
+++ EncompassApi.xUnit.Sample/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Autofac.Extensions.DependencyInjection;
+using EncompassApi.xUnit.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Xunit.DependencyInjection;
+using Xunit.DependencyInjection.Logging;
+
+namespace EncompassApi.xUnit.Sample
+{
+    public partial class Startup
+    {
+        public void ConfigureHost(IHostBuilder hostBuilder) =>
+            hostBuilder.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+
+        public void ConfigureServices(IServiceCollection services) =>
+            services.AddLogging(builder => builder.SetMinimumLevel(LogLevel.Debug))
+            .AddMockedEncompassHttpClient();
+
+        public void Configure(ILoggerFactory loggerFactory, ITestOutputHelperAccessor accessor) =>
+            loggerFactory.AddProvider(new XunitTestOutputLoggerProvider(accessor, delegate { return true; }));
+    }
+}
