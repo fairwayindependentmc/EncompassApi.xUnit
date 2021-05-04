0063c8e02cc61501062eea6f34e2e9152ab3950d
Michael Seifi <michael.seifi@fairwaymc.com>
2021-05-03 20:42:40 -0500

    Add LoanAttachments and MediaUrlObject payload Update Payloads.Helper

---
+++ EncompassApi.xUnit/Payloads/Helper.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace EncompassApi.xUnit.Payloads
+{
+    public static class Helper
+    {
+        public static JObject[] GetArray(string fileName)
+        {
+            var fullPath = $"Payloads/{fileName}.json";
+
+            // Get the absolute path to the JSON file
+            var path = Path.IsPathRooted(fullPath)
+                ? fullPath
+                : Directory.GetCurrentDirectory() + "/" + fullPath;
+            Assert.True(File.Exists(path), $"FIle {fileName}.json doesn't exist!");
+            //if (!File.Exists(path))
+            //{
+                
+            //    throw new ArgumentException($"Could not find file at path: {path}");
+            //}
+            // Load the file
+            var fileData = File.ReadAllText(fullPath);
+            return JsonConvert.DeserializeObject<JObject[]>(fileData);
+        }
+
+        public static JObject[] GetLoanDocuments() => GetArray("LoanDocuments");
+        public static JObject[] GetLoanAttachments() => GetArray("LoanAttachments");
+
+        public static JObject Get(string fileName)
+        {
+            var fullPath = $"Payloads/LoanDocuments.json";
+
+            // Get the absolute path to the JSON file
+            var path = Path.IsPathRooted(fullPath)
+                ? fullPath
+                : Directory.GetCurrentDirectory() + "/" + fullPath;
+            Assert.True(File.Exists(path), $"FIle {fileName}.json doesn't exist!");
+
+            //if (!File.Exists(path))
+            //{
+            //    throw new ArgumentException($"Could not find file at path: {path}");
+            //}
+            // Load the file
+            var fileData = File.ReadAllText(fullPath);
+            var objs = JsonConvert.DeserializeObject<JObject[]>(fileData);
+            return objs[0];
+        }
+
+        public static JObject GetLoanDocument() => Get("LoanDocuments");
+        public static JObject GetMediaUrlObject() => Get("MediaUrlObject");
+    }
+}
