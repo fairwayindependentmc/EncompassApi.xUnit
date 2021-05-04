09b0240369a4c75a73a7643d2a0a63924c2bd9ca
Michael Seifi <michael.seifi@fairwaymc.com>
2021-05-04 16:13:23 -0500

    Add JsonMockConverter

---
+++ EncompassApi.xUnit/Converters/JsonMockConverter.cs
@@ -0,0 +1,28 @@
+﻿using EncompassApi.EFolder;
+using Moq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EncompassApi.xUnit.Converter
+{
+    public static  class JsonMockConverter
+    {
+        public static string ToJson<TObject>(this Mock<TObject> mock) where TObject : class
+        {
+            var objectProperties = typeof(TObject).GetProperties();
+            StringBuilder sb = new StringBuilder();
+            sb.Append("{");
+            foreach (var prop in objectProperties)
+            {
+                sb.Append($"\"{prop.Name}\":\"{mock.Object.GetType().GetProperty(prop.Name).GetValue(mock.Object)}\",");
+            }
+            sb.Remove(sb.Length - 1, 1);
+            sb.Append("}");
+            return sb.ToString();
+        }
+    }
+}
